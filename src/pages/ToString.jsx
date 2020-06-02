e0713fbf0540d81aee5eea1a36354b461f4608a1
haolichong <wb-hlc336023@antfin.com>
2020-06-02 17:57:14 +0800

    feat: reactDOMServer.renderToString()

---
+++ src/pages/ToString.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './ToString.less';
+
+const ToString = () => {
+  const click = () => {
+    console.log(123);
+  };
+  return (
+    <div>
+      <div className={styles.content} onClick={click}  data-xxxx="xxx">
+        我是div
+      </div>
+    </div>
+  );
+};
+
+ToString.label = 'html';
+export default ToString;
