49bf8a53415a9e3a445124cfb59e24a91ca98fc7
haolichong <wb-hlc336023@antfin.com>
2020-06-17 17:49:44 +0800

    feat: 安装resize-wrap包

---
+++ src/pages/ResizeByNPM.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ResizeWrap from 'resize-wrap';
+import LazyLoad from './Lazyload';
+
+const ResizeByNPM = () => {
+  return (
+    <ResizeWrap
+      leftPart={<div>左边</div>}
+      rightPart={<LazyLoad />}
+    />
+  );
+};
+
+ResizeByNPM.label = 'npm上的resize包';
+
+export default ResizeByNPM;
