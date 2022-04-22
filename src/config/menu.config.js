ee3c0ac5d3d9207de57d4bebad0e4223584d645e
haolichong <wb-hlc336023@alibaba-inc.com>
2022-04-22 11:33:10 +0800

    feat: menu生成配置

--- src/config/menu.config.js
+++ src/config/menu.config.js
@@ -1,13 +1,24 @@
 import NotFound from '../components/404';
 
-const context = require.context('../pages', true, /\.(jsx)$/);
-const allKeys = context.keys();
+const context = require.context('../pages', true, /\.((j|t)sx)$/);
+const allKeys = context.keys().filter((k) => {
+  const strArr = k.split('/');
+  if (strArr.length > 2 && !k.includes('index.')) {
+    return false;
+  } else {
+    return true;
+  }
+});
+
 const componentList = [];
 allKeys.forEach((key) => {
   const comp = context(key);
   let path = key;
   if (key.includes('/index.jsx')) {
     path = key.replace('/index.jsx', '');
+  }
+  if (key.includes('/index.tsx')) {
+    path = key.replace('/index.tsx', '');
   }
   componentList.push({
     name: comp.default && (comp.default.label || comp.default.name || path),
