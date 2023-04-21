7f35852f16277a2531d56fd68e60674d14366e3c
秋桐风 <faruxue2019@163.com>
2023-04-22 02:39:45 +0800

    feat: gpt

--- src/pages/ChatGPT/index.tsx
+++ src/pages/ChatGPT/index.tsx
@@ -4,21 +4,36 @@ import axios from 'axios';
 
 const ChatGPT = () => {
   const [text, setText] = useState<string>();
+  const [gptRes, setGptRes] = useState('');
+
+  // app.all('*',function (req, res, next) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Headers', '*');
+  //   res.header('Content-Type', 'application/json;charset=utf-8');
+  //   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  //   next();
+  // });
 
   const onClick = async () => {
-    const res = axios.post(
+    const res = await axios.post(
       'http://ec2-52-91-13-155.compute-1.amazonaws.com:3000/api/chat',
       {
         input: text,
-      },
+      }
     );
     console.log(res);
+    // setGptRes(res.data)
   };
 
   return (
     <div>
       <Input onChange={(e) => setText(e.target.value)} />
       <Button onClick={onClick}>发送</Button>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: gptRes,
+        }}
+      ></div>
     </div>
   );
 };
