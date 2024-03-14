6706cafa2a28641ef6452afa74b1aef8557ce914
伐宿 <fasu@uni-ubi.com>
2024-03-14 15:57:55 +0800

    feat: 圆弧

--- src/pages/DemoPages/HalfCirclePage/index.tsx
+++ src/pages/DemoPages/HalfCirclePage/index.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import HalfCircle from './HalfCircle';
 import styles from './index.module.less';
 
 // 一个虚线的上半圆，每段虚线的长度为50，间隔为5
 const HalfCirclePage = () => {
+  const [currentLevel, setCurrentLevel] = useState(0);
+
+  const onClick = () => {
+    let l = currentLevel + 1;
+    if (l > 5) {
+      l = 0;
+    }
+    setCurrentLevel(l);
+  };
   return (
     <div className={styles.wrap}>
-      <HalfCircle size={200} />
+      <HalfCircle currentLevel={currentLevel} />
+      <button onClick={onClick}>点击</button>
     </div>
   );
 };
